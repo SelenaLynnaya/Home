6dfcaa34fe6202444d72f3f5d55131e96bb217e8
mggtkagu <mggtkagu@mail.ru>
2021-04-26 08:50:03 +0300

    HW

---
+++ HW/HW/Manager.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HW
+{
+    /// <summary>
+    /// Логика взаимодействия для Manager.xaml
+    /// </summary>
+    public partial class Manager : Window
+    {
+        public Manager()
+        {
+            InitializeComponent();
+        }
+
+        private void SaleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Sale sale = new Sale();
+            sale.Show();
+            Close();
+        }
+
+        private void Sort_By_NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sort_By_NameButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HW/HW/Sale.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HW
+{
+    /// <summary>
+    /// Логика взаимодействия для Sale.xaml
+    /// </summary>
+    public partial class Sale : Window
+    {
+        public Sale()
+        {
+            InitializeComponent();
+        }
+
+        private void BackUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager manager = new Manager();
+            manager.Show();
+            Close();
+        }
+
+        private void CreateUserButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackUserButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
