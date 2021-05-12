8cac56948dfff986f6d812afce3ef4cba3acc249
Your Name <you@example.com>
2021-05-12 11:21:25 +0300

    Задание1

--- МягкоеУдаление1.sql
+++ МягкоеУдаление1.sql
@@ -14,7 +14,7 @@ ALTER TABLE Color
 ADD DEL bit default 0;
 GO
 
-ALTER TABLE ColorMaterial
+ALTER TABLE ColorMaterials
 ADD DEL bit default 0;
 GO
 
@@ -23,5 +23,13 @@ ADD DEL bit default 0;
 GO
 
 ALTER TABLE Type
+ADD DEL bit default 0;
+GO
+
+ALTER TABLE LiningMaterial
+ADD DEL bit default 0;
+GO
+
+ALTER TABLE MaterialProduct
 ADD DEL bit default 0;
 GO
